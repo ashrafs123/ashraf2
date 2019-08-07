21e3f6e61cd46ea61588a1d773ec954ec2e0fc53
ashrafs123 <53873338+ashrafs123@users.noreply.github.com>
2019-08-07 21:21:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("608715013636751392")
+setInterval(function() {
+channel.send(`Ashraf_Gamer`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
